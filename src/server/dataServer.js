f99274d863330f4f201c6fc9e4d4084cd6b911af
Elmarino <elmar.atavares@gmail.com>
2018-05-22 16:42:31 +0200

    Add Procfile

--- src/server/dataServer.js
+++ src/server/dataServer.js
@@ -1,6 +1,9 @@
 exports.accountApi = {
-  app_id: '',
-  app_key: ''
+  app_id: '590a27bc',
+  app_key: '8b60c852cc2145d741bdf4a76f2d3e3c'
 };
 
-exports.urlMongoDB = 'mongodb://127.0.0.1:27017/happyfood';
+const mongoID = 'Vince';
+const mongoPwd = 'cookhappy12';
+
+exports.urlMongoDB = 'mongodb://'+mongoID+':'+mongoPwd+'@ds016108.mlab.com:16108/happyfooddb';
