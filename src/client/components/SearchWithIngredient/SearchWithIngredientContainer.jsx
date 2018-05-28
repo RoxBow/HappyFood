fc40ee9e1a5038c066d3fa6984acdc5610a16607
Deplais Vincent <vincent.deplais@orange.fr>
2018-05-28 22:30:07 +0200

    Finish component SearchWithIngredient

--- src/client/components/SearchWithIngredient/SearchWithIngredientContainer.jsx
+++ src/client/components/SearchWithIngredient/SearchWithIngredientContainer.jsx
@@ -1,17 +1,15 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { updateSearch, setResultSearch } from '../../redux/SearchRecipe/action';
+import { updateSearch } from '../../redux/SearchRecipe/action';
 import SearchWithIngredient from './SearchWithIngredient';
 
 const mapStateToProps = state => ({
   textSearch: state.search.text,
-  resultRecipes: state.search.result
 });
 
 const mapDispatchToProps = dispatch => {
   return {
     updateSearch: text => dispatch(updateSearch(text)),
-    setResultSearch: result => dispatch(setResultSearch(result))
   };
 };
 
