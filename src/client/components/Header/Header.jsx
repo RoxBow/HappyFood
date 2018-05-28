0a764f80d5580024cb4e61984e84e7e86bed24b0
Deplais Vincent <vincent.deplais@orange.fr>
2018-05-29 00:37:19 +0200

    Add style home

--- src/client/components/Header/Header.jsx
+++ src/client/components/Header/Header.jsx
@@ -1,37 +1,55 @@
 import '../../styles/_header.scss';
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import { LOGIN, SIGN_UP } from '../../constants';
 
 const Header = ({ isAuthenticated, signUp, showPopin, logout }) => (
-  <header>
-    <h1>HappyFood</h1>
-    <ul className="account-option">
-      {
-        isAuthenticated? (
-          <li className="li-box2">
-            <Link to="/user/me">Go to profile</Link><br/>
-            <button onClick={logout}>Logout</button>
+  <header className="main-header">
+    <div className="top-bar">
+      <h1>
+        <NavLink to="/">
+          Happy <span>Food</span>
+        </NavLink>
+      </h1>
+      <nav>
+        <ul>
+          <li>
+            <NavLink activeClassName="is-active" to="/recipe/allRecipes">
+              Recipes
+            </NavLink>
           </li>
-        ):(
-          <li className="li-box">
-            <button className="login" onClick={() => showPopin(LOGIN)}>Login</button>
-            <button className="register" onClick={() => showPopin(SIGN_UP)}>Register</button>
+          <li>
+            <NavLink activeClassName="is-active" to="/about">
+              About
+            </NavLink>
           </li>
-        )
-      }
-    </ul>
-    <ul className="search-option">
-      <li>
-        <Link to="/searchRecipes">Search Recipes</Link>
-      </li>
-      <li>
-        <Link to="/randomRecipes">Random Recipes</Link>
-      </li>
-      <li>
-        <Link to="/searchWithIngredients">Search with ingredients</Link>
-      </li>
-    </ul>
+
+          {!isAuthenticated ? (
+            <li>
+              <button onClick={() => showPopin(LOGIN)}>Login</button>
+              <button onClick={() => showPopin(SIGN_UP)}>Sign up</button>
+            </li>
+          ) : (
+            <li>
+              <NavLink to="/user/me">My account</NavLink>
+              <button onClick={logout}>Logout</button>
+            </li>
+          )}
+        </ul>
+      </nav>
+    </div>
+    <div className="header-content">
+      <h2>I want to make</h2>
+      <p>Many options</p>
+      <ul>
+        <li>
+          <Link to="/randomRecipes">Random Recipes</Link>
+        </li>
+        <li>
+          <Link to="/searchWithIngredients">Search with ingredients</Link>
+        </li>
+      </ul>
+    </div>
   </header>
 );
 
