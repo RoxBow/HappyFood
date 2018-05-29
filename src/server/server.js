bee07ccad3401a878917c4743e4dc432273f856f
Elmarino <elmar.atavares@gmail.com>
2018-05-29 14:15:46 +0200

    Add recipe to bdd mlab

--- src/server/server.js
+++ src/server/server.js
@@ -18,6 +18,9 @@ const Recipe = require('./models/Recipe');
 
 const port = 3001; // set port server
 
+const { saveRecipeMealDB  } = require('./requestApi/themealdbRequest.js')
+const { saveRecipeEdamam  } = require('./requestApi/edamamRequest.js')
+
 /*
   Add this line before express' response to set CORS header:
   res.header("Access-Control-Allow-Origin", "*");
